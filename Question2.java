b65f86742c0d32eef5b06adc662f35c3a4cb1f40
LingSayKiat <130948597+LingSayKiat@users.noreply.github.com>
2023-04-17 10:58:24 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -11,6 +11,14 @@ public class Question2
      */
      
     Scanner in = new Scanner(System.in);
+    
+    System.out.print("Enter your height in meters: ");
     double height = in.nextDouble();
+    
+    System.out.print("Enter your weight in kilograms: ");
+    double weight = in.nextDouble();
+    
+    double bmi = weight / (height * height);
+    System.out.printf("Your BMI is %.2f", bmi); 
   }
 }
