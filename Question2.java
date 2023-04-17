0e0449805d7f1ca8f02973eac966239fa33ccac3
LingSayKiat <130948597+LingSayKiat@users.noreply.github.com>
2023-04-17 11:02:29 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -19,6 +19,6 @@ public class Question2
     double weight = in.nextDouble();
     
     double bmi = weight / (height * height);
-    System.out.printf("Your BMI is %.2f", bmi); 
+    System.out.print(bmi); 
   }
 }
